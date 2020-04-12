3b4d87c8dde64e1ed67d22344d9ea52fc043de32
Rohit Pandit <fascinating_1987@hotmail.com>
2020-04-12 16:01:08 +0530

    Added Binary Tree Implementation

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -10,6 +10,25 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
+            RunBinarySearchTree();
+            Console.ReadKey();
+        }
+
+        private static void RunBinarySearchTree()
+        {
+            var bst = new TreeNode(10);
+            bst.Insert(4);
+            bst.Insert(16);
+            bst.Insert(17);
+            bst.Insert(1);
+            bst.Insert(2);
+            bst.Insert(15);
+
+            bst.PrintInOrder();
+        }
+
+        private static void RunGraph()
+        {
             var myGraph = new Graph(6);
             myGraph.AddEdge(0, 1);
             myGraph.AddEdge(0, 2);
@@ -21,7 +40,6 @@ namespace DataStructures
             myGraph.AddEdge(4, 5);
             //myGraph.BreadthFirstTraversal(5);
             myGraph.DepthFirstTraversal(0);
-            Console.ReadKey();
         }
     }
 }
---
+++ DataStructures/TreeNode.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class TreeNode
+    {
+        private int _data;
+        private TreeNode _left, _right;
+
+        public TreeNode(int data)
+        {
+            _data = data;
+        }
+
+        public void Insert(int value)
+        {
+            if(value <= _data)
+            {
+                if(_left == null)
+                {
+                    _left = new TreeNode(value);
+                }
+                else
+                {
+                    _left.Insert(value);
+                }
+            }
+            else
+            {
+                if(_right == null)
+                {
+                    _right = new TreeNode(value);
+                }
+                else
+                {
+                    _right.Insert(value);
+                }
+            }
+        }
+
+        public bool Contains(int value)
+        {
+            if (_data == value)
+                return true;
+            else if(value < _data)
+            {
+                if (_left == null)
+                    return false;
+                else
+                    return _left.Contains(value);
+            }
+            else
+            {
+                if (_right == null)
+                    return false;
+                else
+                    return _right.Contains(value);
+            }
+            
+        }
+
+        public void PrintInOrder()
+        {
+            if (_left != null)
+                _left.PrintInOrder();
+            Console.WriteLine(_data.ToString());
+            if (_right != null)
+                _right.PrintInOrder();
+        }
+    }
+}
